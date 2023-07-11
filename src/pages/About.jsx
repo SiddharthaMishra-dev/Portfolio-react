e8bc9189e4bda396b41277ed8c7a94e6086a43f2
Siddhartha-Mishra <mailbox.siddharthamishra@gmail.com>
2023-07-11 13:48:46 +0530

    feat: react-reveal-fade animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Typed from "typed.js";
 import Fade from 'react-reveal/Fade';
+import Jump from 'react-reveal/Jump';
 import Profile from '../assets/Profile.jpg'
 import Nav from '../components/Nav'
 import Footer from "../components/Footer";
@@ -31,79 +32,86 @@ function About(){
     return (
         <div className='m-0 w-full h-screen flex flex-col justify-between'>
             <Nav/>
-            <div className="md:flex bg-slate-800 text-gray-200 lg:w-3/4 m-auto rounded-md shadow-sm items-center md:text-left p-10 md:pt-10">
-                <img src={Profile} className="rounded-md m-auto" width="384" height="512" ></img>
-                <div className="side-content p-10">
-                    <h4 className="text-2xl">Hey!</h4>
-                    <p className="text-sky-500 text-5xl mt-4">Siddhartha Mishra</p>
-                    <p className="text-gray-500 text-3xl mt-5" ref={desc}/>
-                    <div className="text-lg mt-4">
-                        I am an undergraduate student with a passion for web development. Enjoy exploring new technologies 
-                        and using them. Always eager to work and collaborate with senior and junior developers. Enthusiastic 
-                        about learning and applying new technologies to provide the best solution.
+            <Fade left>
+                <div className="md:flex bg-slate-800 text-gray-200 lg:w-3/4 m-auto rounded-md shadow-sm items-center md:text-left p-10 md:pt-10">
+                    <img src={Profile} className="rounded-md m-auto" width="384" height="512" ></img>
+                    <div className="side-content p-10">
+                        <h4 className="text-2xl">Hey!</h4>
+                        <p className="text-sky-500 text-5xl mt-4">Siddhartha Mishra</p>
+                        <p className="text-gray-500 text-3xl mt-5" ref={desc}/>
+                        <div className="text-lg mt-4">
+                            I am an undergraduate student with a passion for web development. Enjoy exploring new technologies 
+                            and using them. Always eager to work and collaborate with senior and junior developers. Enthusiastic 
+                            about learning and applying new technologies to provide the best solution.
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fade>
+            <Fade right>
+                <div className="technologies text-gray-200 p-10 lg:w-3/4 m-auto mt-32 mb-32 bg-slate-800 rounded-md">
+                    <h3 className="text-5xl text-center m-5"><span className="active">Tech I use</span></h3>
+                    <div className="flex gap-36 w-1/2 m-auto p-10 justify-center">
+                        <div>
+                            <Fade left>
+                                <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500">
+                                    <IoLogoJavascript/>
+                                </div>
+                                <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><FaBootstrap/></div>
+                                <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><FaReact/></div>
+                            </Fade>
+                        </div>
+                        <div>
+                            <Fade right>
+                                <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><SiTailwindcss/></div>
+                                <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><SiExpress/></div>
+                                <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><FaNodeJs/></div>
+                            </Fade>
+                        </div>
+                    </div>
+                </div>
+            </Fade>
+            <Fade left>
+                <div className="experience text-gray-200 lg:w-3/4 m-auto  mb-32 rounded-md shadow-sm items-center md:text-left p-10 md:pt-10">
+                    <h1 className="text-5xl text-center m-5">Experience</h1>
+                        <div className="flex gap-5 justify-center p-0 md:p-5">
+                            {
+                                experience.map((job,index1)=>{
+                                    return(
+                                        <p key={index1} onClick={()=>handleChange(job.id)} className={`p-1 text-xl cursor-pointer hover:text-sky-500 ${job.id === index && 'active'}`}>
+                                            {job.company}
+                                        </p>
+                                    )
+                                })
+                            }
+                        </div >
+                        <div className="job-item p-5 bg-slate-800 rounded-md mt-5">
+                            <p className="text-2xl text-sky-500 font-bold"><span className="text-gray-200">{experience[index].designation}</span> @{experience[index].company}</p>
+                            <p className="mt-2 text-gray-400">{experience[index].tenure}</p>
+                            <article className="mt-2">
+                                {experience[index].description}
+                            </article>
+                        </div>
+                </div>
+            </Fade>
+            <Fade right>
+                <div className="connect text-gray-200 p-10  mb-32"> 
+                    <h3 className="text-5xl text-center m-5"><span className="active">Connect with me</span></h3>
+                    <div className="follow flex flex-col items-center">
+                        <p className="p-5 text-slate-400">Follow me on </p>
+                        <ul className="flex gap-5 p-5">
+                            <li className="text-6xl hover:text-sky-500 cursor-pointer hover:-translate-y-1 duration-300"><a href="https://twitter.com/raiderfreed"><FaTwitter/></a></li>
+                            <li className="text-6xl hover:text-sky-500 cursor-pointer hover:-translate-y-1 duration-300"><a href="https://www.linkedin.com/in/siddhartha-mishra-628282203/"><FaLinkedin/></a></li>
+                            <li className="text-6xl hover:text-sky-500 cursor-pointer hover:-translate-y-1 duration-300"><a href="https://www.instagram.com/sid_mishra19/" target="_blank"><FaInstagram/></a></li>
 
-            <div className="technologies text-gray-200 p-10 lg:w-3/4 m-auto mt-32 mb-32 bg-slate-800 rounded-md">
-                <h3 className="text-5xl text-center m-5"><span className="active">Tech I use</span></h3>
-                <div className="flex gap-36 w-1/2 m-auto p-10 justify-center">
-                    <div>
-                        <Fade left>
-                            <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500">
-                                <IoLogoJavascript/>
+                        </ul>
+                        <Jump>
+                            <div className="bg-sky-500 hover:bg-sky-600 p-4 m-5 rounded-md transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer">
+                                <h4 className="text-xl "><a href="mailto:mailbox.siddharthamishra@gmail.com?body=Add your body here">Mail me</a></h4>
                             </div>
-                            <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><FaBootstrap/></div>
-                            <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><FaReact/></div>
-                        </Fade>
-                    </div>
-                    <div>
-                        <Fade right>
-                            <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><SiTailwindcss/></div>
-                            <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><SiExpress/></div>
-                            <div className="p-3 text-4xl  hover:-translate-y-1 duration-300 hover:text-sky-500"><FaNodeJs/></div>
-                        </Fade>
+                        </Jump>
                     </div>
                 </div>
-            </div>
-
-            <div className="experience text-gray-200 lg:w-3/4 m-auto  mb-32 rounded-md shadow-sm items-center md:text-left p-10 md:pt-10">
-                <h1 className="text-5xl text-center m-5">Experience</h1>
-                    <div className="flex gap-5 justify-center p-0 md:p-5">
-                        {
-                            experience.map((job,index1)=>{
-                                return(
-                                    <p key={index1} onClick={()=>handleChange(job.id)} className={`p-1 text-xl cursor-pointer hover:text-sky-500 ${job.id === index && 'active'}`}>
-                                        {job.company}
-                                    </p>
-                                )
-                            })
-                        }
-                    </div >
-                    <div className="job-item p-5 bg-slate-800 rounded-md mt-5">
-                        <p className="text-2xl text-sky-500 font-bold"><span className="text-gray-200">{experience[index].designation}</span> @{experience[index].company}</p>
-                        <p className="mt-2 text-gray-400">{experience[index].tenure}</p>
-                        <article className="mt-2">
-                            {experience[index].description}
-                        </article>
-                    </div>
-            </div>
-
-            <div className="connect text-gray-200 p-10  mb-32"> 
-                <h3 className="text-5xl text-center m-5"><span className="active">Connect with me</span></h3>
-                <div className="follow flex flex-col items-center">
-                    <p className="p-5 text-slate-400">Follow me on </p>
-                    <ul className="flex gap-5 p-5">
-                        <li className="text-6xl hover:text-sky-500 cursor-pointer hover:-translate-y-1 duration-300"><a href="https://twitter.com/raiderfreed"><FaTwitter/></a></li>
-                        <li className="text-6xl hover:text-sky-500 cursor-pointer hover:-translate-y-1 duration-300"><a href="https://www.linkedin.com/in/siddhartha-mishra-628282203/"><FaLinkedin/></a></li>
-                        <li className="text-6xl hover:text-sky-500 cursor-pointer hover:-translate-y-1 duration-300"><a href="https://www.instagram.com/sid_mishra19/" target="_blank"><FaInstagram/></a></li>
-
-                    </ul>
-                    <div className="bg-sky-500 hover:bg-sky-600 p-4 m-5 rounded-md transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer">
-                        <h4 className="text-xl "><a href="mailto:mailbox.siddharthamishra@gmail.com?body=Add your body here">Mail me</a></h4>
-                    </div>
-                </div>
-            </div>
+            </Fade>
 
             <Footer/>
         </div>
